a5194fb181227356ce764376e4720680c7f52c65
Adam Barton <adam.barton333@gmail.com>
2018-10-02 23:27:46 +0000

    automatically backed up by learn

--- db/migrate/008_update_characters_catchphrase.rb
+++ db/migrate/008_update_characters_catchphrase.rb
@@ -1,4 +1,4 @@
-class NextUpdateCharacters < ActiveRecord::Migration[4.2]
+class NextUpdateCharactersCatchphrase < ActiveRecord::Migration[4.2]
 
   def change
     add_column :characters, :catchphrase, :string
